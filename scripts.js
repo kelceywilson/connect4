2f454a4038157832cf1c9f33a65356a5cc91a05d
Kelcey Wilson <kelceywilson@gmail.com>
2019-05-21 16:28:14 -0700

    Add findRow and addToken functions

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 /**
- * Builds matrix of zeros representing a new board
+ * Creates new board
+ *
+ * @param {number} [height=6]
+ * @param {number} [width=7]
  * @returns Array
  */
-function newBoard() {
+function newBoard(height = 6, width = 7) {
   const matrix = [];
-  const height = 6;
-  const width = 7;
 
   for (let y = 0; y < height; y++) {
     matrix[y] = [];
@@ -16,4 +17,39 @@ function newBoard() {
   return matrix;
 }
 
-console.log(newBoard());
+/**
+ * Finds first empty row in selected column
+ *
+ * @param {*} board
+ * @param {*} column
+ * @returns Integer
+ */
+function findRow(board, column) {
+  let row;
+  // Refactor for performance & best practices?
+  for (row = board.length - 1; row >= 0; row--) {
+    if (board[row][column] === 0) {
+      return row;
+    } else {
+      return "row already full";
+    }
+  }
+  return row;
+}
+
+/**
+ * Places current player's token on board
+ *
+ * @param {*} board
+ * @param {*} player
+ * @param {*} column
+ * @param {*} row
+ */
+function addToken(board, player, column, row) {
+  player === 1 ? 1 : 2;
+  board[row][column] = player;
+  return board;
+}
+
+const board = newBoard();
+console.log(board);
